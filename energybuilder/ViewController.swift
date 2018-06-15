7a97323cf99af937eee95f12321cacb08722aba1
theptokim <kimthepit@gmail.com>
2018-06-15 10:12:13 +0700

    Initial Commit

---
+++ energybuilder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  energybuilder
+//
+//  Created by Thep To Kim on 6/15/18.
+//  Copyright © 2018 Thep To Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
